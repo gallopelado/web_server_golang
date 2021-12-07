1e62c1fd7282b6ec7a43301b61aa2320fff61759
gallopelado <juanftp100@gmail.com>
2021-12-07 16:53:10 -0300

    add: implementando json

--- src/handlers.go
+++ src/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -12,4 +13,34 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleHome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test home page")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata Metadata
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v\n", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		//fmt.Println(user.Name)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//fmt.Fprintf(w, "Payload %v\n", data)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
